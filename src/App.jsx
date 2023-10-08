d9f7ab076a7fa51609d8376164f6e60464b24c9e
Lucas Ybañez <luck.facebook@hotmail.com>
2023-10-08 02:22:45 -0300

    mis reservas: pagina de reservas filtradas

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
-import { useState } from 'react'
-import Navbar from './components/navbar'
-import { ReservasProvider } from './context/contexto'
-import Footer from "./components/footer/Footer";
-import Header from "./components/navbar/Header";
+
 import "bootstrap/dist/css/bootstrap.min.css";
-import UserContext from "./context/UserContext";
-import "./i18n";
-//import { useContext, useState } from "react";
-import { NavbarContext, NavbarContexto } from "./context/NavbarContext.jsx";
-import Rutas from "./routes/Rutas";
+import MisReservas from "./pages/Mis Reservas/MisReservas";
 
 function App() {
 
   return (
     <>
-      <NavbarContexto>
-        <UserContext>
-           <Header/> 
-          <Rutas />
-      <Footer/>
-        </UserContext>
-      </NavbarContexto>
+            <MisReservas></MisReservas>
+
     </>
   );
 }
